8a1c2fdcb537745b273ead6d94419e6e61424baf
del <dineshdel510@gamil.com>
2023-11-28 21:26:24 +0530

    RemovedVerifyToken

--- routes/posts.js
+++ routes/posts.js
@@ -21,7 +21,7 @@ router.post("/create", async (req, res) => {
 });
 
 //UPDATE
-router.put("/:id", verifyToken, async (req, res) => {
+router.put("/:id", async (req, res) => {
   try {
     const updatedPost = await Post.findByIdAndUpdate(
       req.params.id,
@@ -35,7 +35,7 @@ router.put("/:id", verifyToken, async (req, res) => {
 });
 
 //DELETE
-router.delete("/:id", verifyToken, async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     await Post.findByIdAndDelete(req.params.id);
     await Comment.deleteMany({ postId: req.params.id });
