fdc7ded4922694fb06d927acf99f452157a1e60c
del <dineshdel510@gamil.com>
2023-12-04 16:24:28 +0530

    Cors changes

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const connectDB = async () => {
 dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
